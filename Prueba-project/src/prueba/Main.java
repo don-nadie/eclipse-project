89a25899863a9adab8eebd68bf7941f7589f16cd
rbatsyatsyny <rbatsyatsyny@DXCESPAI693066.emea.globalcsc.net>
2018-09-04 17:53:53 +0200

    Nuevo Proyecto de prueba

---
+++ Prueba-project/src/prueba/Main.java
@@ -0,0 +1,19 @@
+package prueba;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Hola mundo!");
+		System.out.print("Tu nombre: ");
+		String nombre = entrada.nextLine();
+		System.out.println("Que te den, " + nombre + "!!");
+		
+		entrada.close();
+	}
+
+}
